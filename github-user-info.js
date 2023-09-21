713a7b83ac79e48d6d262ec55a65ab64227a6d45
JAK-Owen <64730020+JAK-Owen@users.noreply.github.com>
2023-09-21 15:22:54 +0100

    Code formatting and commenting

--- github-user-info.js
+++ github-user-info.js
@@ -1,54 +1,85 @@
+// This is a Vue.js component for displaying GitHub user information.
 export const GitHubUserInfo = {
-    template: `
-      <div>
-        <input v-model="username" placeholder="Enter GitHub username" @input="searchUser" />
-        <div v-if="user">
-          <h2>{{ user.name || 'Name not available' }}</h2>
-          <p>Profile Name: {{ user.login || 'Profile name not available' }}</p>
-          <p>Bio: {{ user.bio || 'Bio not available' }}</p>
-          <p>Date Joined: {{ formatDate(user.created_at) || 'Date joined not available' }}</p>
-          <p>Number of Repos: {{ user.public_repos || 'Repos not available' }}</p>
-          <p>Number of Followers: {{ user.followers || 'Followers not available' }}</p>
-          <p>Number Following: {{ user.following || 'Following not available' }}</p>
-          <p>Location: {{ user.location || 'Location not available' }}</p>
-          <p>Twitter: <a :href="'https://twitter.com/' + user.twitter_username">{{ user.twitter_username || 'Twitter not available' }}</a></p>
-          <p>Website: <a :href="user.blog || '#'">{{ user.blog || 'Website not available' }}</a></p>
-          <p>Company: {{ user.company || 'Company not available' }}</p>
-        </div>
-        <div v-else-if="loading">Loading...</div>
-        <div v-else-if="error">User not found</div>
+  // HTML template for the component
+  template: `
+    <div>
+      <!-- Input field for GitHub username -->
+      <input v-model="username" placeholder="Enter GitHub username" @input="searchUser" />
+      <!-- Display user information or loading/error messages -->
+      <div v-if="user">
+        <!-- User's name, fallback to 'Name not available' if not provided -->
+        <h2>{{ user.name || 'Name not available' }}</h2>
+        <!-- User's profile name, fallback to 'Profile name not available' -->
+        <p>Profile Name: {{ user.login || 'Profile name not available' }}</p>
+        <!-- User's bio, fallback to 'Bio not available' -->
+        <p>Bio: {{ user.bio || 'Bio not available' }}</p>
+        <!-- Date joined GitHub, fallback to 'Date joined not available' -->
+        <p>Date Joined: {{ formatDate(user.created_at) || 'Date joined not available' }}</p>
+        <!-- Number of public repositories, fallback to 'Repos not available' -->
+        <p>Number of Repos: {{ user.public_repos || 'Repos not available' }}</p>
+        <!-- Number of followers, fallback to 'Followers not available' -->
+        <p>Number of Followers: {{ user.followers || 'Followers not available' }}</p>
+        <!-- Number following, fallback to 'Following not available' -->
+        <p>Number Following: {{ user.following || 'Following not available' }}</p>
+        <!-- User's location, fallback to 'Location not available' -->
+        <p>Location: {{ user.location || 'Location not available' }}</p>
+        <!-- User's Twitter handle with a link, fallback to 'Twitter not available' -->
+        <p>Twitter: <a :href="'https://twitter.com/' + user.twitter_username">{{ user.twitter_username || 'Twitter not available' }}</a></p>
+        <!-- User's website with a link, fallback to '#' if not provided -->
+        <p>Website: <a :href="user.blog || '#'">{{ user.blog || 'Website not available' }}</a></p>
+        <!-- User's company, fallback to 'Company not available' -->
+        <p>Company: {{ user.company || 'Company not available' }}</p>
       </div>
-    `,
-    data() {
-      return {
-        username: "",
-        user: null,
-        loading: false,
-        error: false,
-      };
-    },
-    methods: {
-      async searchUser() {
-        this.loading = true;
-        this.error = false;
-        try {
-          const response = await fetch(`https://api.github.com/users/${this.username}`);
-          if (response.ok) {
-            this.user = await response.json();
-          } else {
-            this.error = true;
-          }
-        } catch (err) {
-          console.error(err);
+      <!-- Loading message while fetching data -->
+      <div v-else-if="loading">Loading...</div>
+      <!-- Error message if the user is not found or an error occurs -->
+      <div v-else-if="error">User not found</div>
+    </div>
+  `,
+  // Data properties for the component
+  data() {
+    return {
+      // Stores the entered GitHub username
+      username: "",
+      // Stores the fetched user data
+      user: null,
+      // Indicates if data is being fetched
+      loading: false,
+      // Indicates if there's an error
+      error: false,
+    };
+  },
+  // Methods for the component
+  methods: {
+    // Method to fetch GitHub user data
+    async searchUser() {
+      // Show loading message
+      this.loading = true;
+      // Reset error flag
+      this.error = false;
+      try {
+        // Fetch user data from GitHub API using the provided username
+        const response = await fetch(`https://api.github.com/users/${this.username}`);
+        if (response.ok) {
+          // If successful, store the user data
+          this.user = await response.json();
+        } else {
+          // If not successful, set the error flag
           this.error = true;
-        } finally {
-          this.loading = false;
         }
-      },
-      formatDate(date) {
-        const options = { year: 'numeric', month: 'long', day: 'numeric' };
-        return new Date(date).toLocaleDateString(undefined, options);
-      },
+      } catch (err) {
+        // Handle any errors and set the error flag
+        console.error(err);
+        this.error = true;
+      } finally {
+        // Hide loading message when data fetching is complete
+        this.loading = false;
+      }
     },
-  };
-  
+    // Method to format a date string as a user-friendly date
+    formatDate(date) {
+      const options = { year: 'numeric', month: 'long', day: 'numeric' };
+      return new Date(date).toLocaleDateString(undefined, options);
+    },
+  },
+};
--- script.js
+++ script.js
@@ -1,41 +1,50 @@
+// Import necessary Vue.js modules and a custom component
 import { createApp, ref, onMounted } from 'https://unpkg.com/vue@3.2.7/dist/vue.esm-browser.js';
 import { GitHubUserInfo } from './github-user-info.js';
 
+// Create a Vue.js application
 const app = createApp({
   setup() {
-    // Use a ref to track the 'username' and 'user' data
-    const defaultUsername = 'octocat'; // Default username
-    const username = ref(''); // Initialize username as an empty string
+    // Initialize default GitHub username
+    const defaultUsername = 'octocat';
 
+    // Track user input for GitHub username
+    const username = ref('');
+
+    // Store user data fetched from GitHub
     const user = ref(null);
 
-    // Function to search for the user
+    // Function to fetch user data from GitHub API
     async function searchUser() {
       user.value = null; // Clear previous user data
       const response = await fetch(`https://api.github.com/users/${username.value || defaultUsername}`);
       if (response.ok) {
-        user.value = await response.json();
+        user.value = await response.json(); // Update user data if request is successful
       }
     }
 
-    // Function to format date
+    // Function to format dates
     function formatDate(date) {
       const options = { year: 'numeric', month: 'long', day: 'numeric' };
       return new Date(date).toLocaleDateString(undefined, options);
     }
 
-    // Call the searchUser function on component creation
+    // Call the searchUser function when the component is mounted
     onMounted(searchUser);
 
+    // Return data and functions for the Vue.js component
     return {
-      username,
-      user,
-      searchUser,
-      formatDate,
-      defaultUsername,
+      username,       // User input field
+      user,           // GitHub user data
+      searchUser,     // Function to fetch user data
+      formatDate,     // Function to format dates
+      defaultUsername, // Default GitHub username
     };
   },
 });
 
+// Register a custom component for displaying GitHub user info
 app.component("github-user-info", GitHubUserInfo);
+
+// Mount the Vue.js application to an HTML element with the ID 'app'
 app.mount("#app");
