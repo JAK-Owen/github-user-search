b079a966c41e80f80f8736d8fd06d259a0706ff3
JAK-Owen <64730020+JAK-Owen@users.noreply.github.com>
2023-09-21 11:35:39 +0100

    Set default user info to octocat and removed some elements for rebuilding

--- github-user-info.js
+++ github-user-info.js
@@ -1,55 +1,54 @@
 export const GitHubUserInfo = {
-    props: ['username'], // Define the 'username' prop
     template: `
       <div>
         <input v-model="username" placeholder="Enter GitHub username" @input="searchUser" />
         <div v-if="user">
-          <img :src="user.avatar_url" alt="User Avatar" class="circular-image" />
-          <h2>{{ user.name }}</h2>
-          <p>Profile Name: {{ user.login }}</p>
-          <p>Bio: {{ user.bio }}</p>
-          <p>Date Joined: {{ formatDate(user.created_at) }}</p>
-          <p>Number of Repos: {{ user.public_repos }}</p>
-          <p>Number of Followers: {{ user.followers }}</p>
-          <p>Number Following: {{ user.following }}</p>
-          <p>Location: {{ user.location }}</p>
-          <p v-if="user.twitter_username">Twitter: <a :href="'https://twitter.com/' + user.twitter_username">{{ user.twitter_username }}</a></p>
-          <p v-if="user.blog">Website: <a :href="user.blog">{{ user.blog }}</a></p>
-          <p>Company: {{ user.company }}</p>
+          <h2>{{ user.name || 'Name not available' }}</h2>
+          <p>Profile Name: {{ user.login || 'Profile name not available' }}</p>
+          <p>Bio: {{ user.bio || 'Bio not available' }}</p>
+          <p>Date Joined: {{ formatDate(user.created_at) || 'Date joined not available' }}</p>
+          <p>Number of Repos: {{ user.public_repos || 'Repos not available' }}</p>
+          <p>Number of Followers: {{ user.followers || 'Followers not available' }}</p>
+          <p>Number Following: {{ user.following || 'Following not available' }}</p>
+          <p>Location: {{ user.location || 'Location not available' }}</p>
+          <p>Twitter: <a :href="'https://twitter.com/' + user.twitter_username">{{ user.twitter_username || 'Twitter not available' }}</a></p>
+          <p>Website: <a :href="user.blog || '#'">{{ user.blog || 'Website not available' }}</a></p>
+          <p>Company: {{ user.company || 'Company not available' }}</p>
         </div>
         <div v-else-if="loading">Loading...</div>
         <div v-else-if="error">User not found</div>
       </div>
     `,
     data() {
-        return {
-            username: "",
-            user: null,
-            loading: false,
-            error: false,
-        };
+      return {
+        username: "",
+        user: null,
+        loading: false,
+        error: false,
+      };
     },
     methods: {
-        async searchUser() {
-            this.loading = true;
-            this.error = false;
-            try {
-                const response = await fetch(`https://api.github.com/users/${this.username}`);
-                if (response.ok) {
-                    this.user = await response.json();
-                } else {
-                    this.error = true;
-                }
-            } catch (err) {
-                console.error(err);
-                this.error = true;
-            } finally {
-                this.loading = false;
-            }
-        },
-        formatDate(date) {
-            const options = { year: 'numeric', month: 'long', day: 'numeric' };
-            return new Date(date).toLocaleDateString(undefined, options);
-        },
+      async searchUser() {
+        this.loading = true;
+        this.error = false;
+        try {
+          const response = await fetch(`https://api.github.com/users/${this.username}`);
+          if (response.ok) {
+            this.user = await response.json();
+          } else {
+            this.error = true;
+          }
+        } catch (err) {
+          console.error(err);
+          this.error = true;
+        } finally {
+          this.loading = false;
+        }
+      },
+      formatDate(date) {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        return new Date(date).toLocaleDateString(undefined, options);
+      },
     },
-};
+  };
+  
--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-import { createApp, ref } from 'https://unpkg.com/vue@3.2.7/dist/vue.esm-browser.js';
+import { createApp, ref, onMounted } from 'https://unpkg.com/vue@3.2.7/dist/vue.esm-browser.js';
 import { GitHubUserInfo } from './github-user-info.js';
 
 const app = createApp({
   setup() {
     // Use a ref to track the 'username' and 'user' data
-    const username = ref('');
+    const defaultUsername = 'octocat'; // Default username
+    const username = ref(''); // Initialize username as an empty string
+
     const user = ref(null);
 
     // Function to search for the user
     async function searchUser() {
       user.value = null; // Clear previous user data
-      const response = await fetch(`https://api.github.com/users/${username.value}`);
+      const response = await fetch(`https://api.github.com/users/${username.value || defaultUsername}`);
       if (response.ok) {
         user.value = await response.json();
       }
@@ -22,11 +24,15 @@ const app = createApp({
       return new Date(date).toLocaleDateString(undefined, options);
     }
 
+    // Call the searchUser function on component creation
+    onMounted(searchUser);
+
     return {
       username,
       user,
       searchUser,
       formatDate,
+      defaultUsername,
     };
   },
 });
